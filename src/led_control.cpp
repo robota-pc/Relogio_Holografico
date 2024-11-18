fc44e97feaabb5180faa3ea5ef114883dd2b7548
Leo F <leonardoaguiarforner@gmail.com>
2024-11-18 09:47:46 -0300

    exibicao comeca e termina no sensor

--- src/led_control.cpp
+++ src/led_control.cpp
@@ -11,72 +11,77 @@ int j = 0;
 int green = 0, red = 0, blue = 0;
 
 void ledloop(){
-    if (currentMicros >= (tempo + t_arco)) {
-        tempo += t_arco;
-        
-        for (int i = LED_COUNT - 1; i >= 0; i--) 
-        {
-            if (sessoes > 0){
-                if ( j%2 == 0){
-                    strip.setPixelColor(i, strip.Color(255, 0, 0));
+    if (detect > 0) {
+        j = 0;
+    } else {
+        if (currentMicros >= (tempo + t_arco)) {
+            tempo += t_arco;
+            
+            for (int i = LED_COUNT - 1; i >= 0; i--) 
+            {
+                if (sessoes > 0){
+                    if(j > sessoes) j = 1;
+                    
+                    if ( j%2 == 0){
+                        strip.setPixelColor(i, strip.Color(255, 0, 0));
+                    } else {
+                        strip.setPixelColor(i, strip.Color(0, 0, 255));
+                    }
+
                 } else {
-                    strip.setPixelColor(i, strip.Color(0, 0, 255));
+                        
+                    if (i > (LED_COUNT - cima - 1)) {
+                        strip.setPixelColor(i, strip.Color(75, 0, 130));
+                        
+                    } else if(i > baixo)
+                    {
+                        
+                        estado = j + (LED_COUNT - i - baixo ) * largura;
+                        red = imagem1[estado][0];
+                        green = imagem1[estado][1];
+                        blue = imagem1[estado][2];
+                        
+                        /*
+                        if(green < 150){
+                        green = 0;
+                        }else {
+                        green = 250;
+                        }
+                        if(red < 125){
+                        red = 0;
+                        }else {
+                        red = 250;
+                        }
+                        if(blue <= 125){
+                        blue = 0;
+                        }else {
+                        blue = 250;
+                        }
+                        */
+                        
+                        
+                        if ((blue >= 250) or (red >= 250) or (green >= 250)) {
+                        blue = 0;
+                        red = 0;
+                        green = 0;
+                        } //else {
+                        //red = 255;
+                        // green = 25;
+                        //blue = 0;
+                        //}
+                        
+                        strip.setPixelColor( i , strip.Color(red, green, blue));
+                    } else {
+                        strip.setPixelColor(i, strip.Color(255, 25, 0));
+                    }
                 }
                 
-
-            } else {
-                    
-                if (i > (LED_COUNT - cima - 1)) {
-                    strip.setPixelColor(i, strip.Color(75, 0, 130));
-                    
-                } else if(i > baixo)
-                {
-                    
-                    estado = j + (LED_COUNT - i - baixo ) * largura;
-                    red = imagem1[estado][0];
-                    green = imagem1[estado][1];
-                    blue = imagem1[estado][2];
-                    
-                    /*
-                    if(green < 150){
-                    green = 0;
-                    }else {
-                    green = 250;
-                    }
-                    if(red < 125){
-                    red = 0;
-                    }else {
-                    red = 250;
-                    }
-                    if(blue <= 125){
-                    blue = 0;
-                    }else {
-                    blue = 250;
-                    }
-                    */
-                    
-                    
-                    if ((blue >= 250) or (red >= 250) or (green >= 250)) {
-                    blue = 0;
-                    red = 0;
-                    green = 0;
-                    } //else {
-                    //red = 255;
-                    // green = 25;
-                    //blue = 0;
-                    //}
-                    
-                    strip.setPixelColor( i , strip.Color(red, green, blue));
-                } else {
-                    strip.setPixelColor(i, strip.Color(255, 25, 0));
-                }
             }
-            
-        }
-        strip.show();  
-        j += 1;
-        if(j == largura - 1){
-            j = 0;
+            strip.show();  
+            j += 1;
+            if(j == largura - 1){
+                j = 0;
+            }
         }
     }
 }
